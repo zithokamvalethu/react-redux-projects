ba1a9fa02fe2fbb64f00aa1a6bebc7cdf10b0ad2
zithokamvalethu <kamvalethu.zitho1705@gmail.com>
2022-03-14 10:20:22 +0200

    added two elements for input and displaying

--- random/markdownPreviewer/App.js
+++ random/markdownPreviewer/App.js
@@ -1,7 +1,13 @@
 import React from "react";
 
 function MarkDownPreviewer() {
-  return <div>Mark Down Previewer</div>;
+  return (
+    <div>
+      Mark Down Previewer
+      <textarea id="editor"></textarea>
+      <ul id="preview"></ul>
+    </div>
+  );
 }
 
 export default MarkDownPreviewer;
