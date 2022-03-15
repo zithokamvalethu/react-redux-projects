81db4f2cefc75a73cbc9e9a14e55968449e99f96
zithokamvalethu <kamvalethu.zitho1705@gmail.com>
2022-03-15 12:34:05 +0200

    added changes for the drum machine

--- Drum Machine/index.js
+++ Drum Machine/index.js
@@ -1,97 +1,96 @@
-function playAudio(K) {
-  var audio = document.getElementById(K);
-  audio.play(K);
-}
+// function playAudio(K) {
+//   var audio = document.getElementById(K);
+//   audio.play(K);
+// }
 
-function pauseAudio() {
-  var audio = document.getElementById("K");
-  audio.pause();
-}
+// function pauseAudio() {
+//   var audio = document.getElementById("K");
+//   audio.pause();
+// }
 
-function playAudio(A) {
-  var audio = document.getElementById(A);
-  audio.play(A);
-}
+// function playAudio(A) {
+//   var audio = document.getElementById(A);
+//   audio.play(A);
+// }
 
-function pauseAudio() {
-  var audio = document.getElementById("A");
-  audio.pause();
-}
+// function pauseAudio() {
+//   var audio = document.getElementById("A");
+//   audio.pause();
+// }
 
-function playAudio(M) {
-  var audio = document.getElementById(M);
-  audio.play(M);
-}
+// function playAudio(M) {
+//   var audio = document.getElementById(M);
+//   audio.play(M);
+// }
 
-function pauseAudio() {
-  var audio = document.getElementById("M");
-  audio.pause();
-}
+// function pauseAudio() {
+//   var audio = document.getElementById("M");
+//   audio.pause();
+// }
 
-function playAudio(V) {
-  var audio = document.getElementById(V);
-  audio.play(V);
-}
+// function playAudio(V) {
+//   var audio = document.getElementById(V);
+//   audio.play(V);
+// }
 
-function pauseAudio() {
-  var audio = document.getElementById("V");
-  audio.pause();
-}
+// function pauseAudio() {
+//   var audio = document.getElementById("V");
+//   audio.pause();
+// }
 
-function playAudio(L) {
-  var audio = document.getElementById(L);
-  audio.play(L);
-}
+// function playAudio(L) {
+//   var audio = document.getElementById(L);
+//   audio.play(L);
+// }
 
-function pauseAudio() {
-  var audio = document.getElementById("L");
-  audio.pause();
-}
-function playAudio(E) {
-  var audio = document.getElementById(E);
-  audio.play(E);
-}
-function pauseAudio() {
-  var audio = document.getElementById("E");
-  audio.pause();
-}
+// function pauseAudio() {
+//   var audio = document.getElementById("L");
+//   audio.pause();
+// }
+// function playAudio(E) {
+//   var audio = document.getElementById(E);
+//   audio.play(E);
+// }
+// function pauseAudio() {
+//   var audio = document.getElementById("E");
+//   audio.pause();
+// }
 
-function playAudio(T) {
-  var audio = document.getElementById(T);
-  audio.play(T);
-}
+// function playAudio(T) {
+//   var audio = document.getElementById(T);
+//   audio.play(T);
+// }
 
-function pauseAudio() {
-  var audio = document.getElementById("T");
-  audio.pause();
-}
-function playAudio(H) {
-  var audio = document.getElementById(H);
-  audio.play(H);
-}
+// function pauseAudio() {
+//   var audio = document.getElementById("T");
+//   audio.pause();
+// }
+// function playAudio(H) {
+//   var audio = document.getElementById(H);
+//   audio.play(H);
+// }
 
-function pauseAudio() {
-  var audio = document.getElementById("H");
-  audio.pause();
-}
-// let sound = null;
-// const playsound = (event) => {
-//   if (sound !== null) {
-//     sound.pause();
-//     sound.currentTime = 0;
-//   }
-//   id = event.target.value;
-//   console.log(id);
-//   if (id === "K") id = "K";
-//   if (id === "A") id = "A";
-//   sound = document.getElementById(id);
-//   console.log(sound);
-//   //   var instrument =
-//   //     sound === null && sound.src.split("/")[sound.src.split("/").length - 1];
-//   //   document.getElementById("drum-machine").innerHTML = instrument.replace(
-//   //     ".mp3",
-//   //     ""
-//   //   );
-//   sound.play();
-// };
-// window.addEventListener("click", playsound);
+// function pauseAudio() {
+//   var audio = document.getElementById("H");
+//   audio.pause();
+// }
+let sound = null;
+const playsound = (event) => {
+  if (sound !== null) {
+    sound.pause();
+    sound.currentTime = 0;
+  }
+  id = event.target.value;
+  if (id === "Q") id = "Q";
+  if (id === " W") id = "W";
+  sound = document.getElementById(id);
+
+  //   var instrument =
+  //     sound === null && sound.src.split("/")[sound.src.split("/").length - 1];
+  //   document.getElementById("display").innerHTML = instrument.replace(
+  //     ".mp3",
+  //     ""
+  //   );
+  sound.play();
+};
+window.addEventListener("click", playsound);
